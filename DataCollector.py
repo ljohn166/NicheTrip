cdd64919eaf3cae24f7f4a31e238ce2624034227
gabogiapp <129702410+gabogiapp@users.noreply.github.com>
2025-10-05 11:51:03 -0400

    fix

--- DataCollector.py
+++ DataCollector.py
@@ -5,7 +5,7 @@ import os
 import time
 
 
-#load_dotenv()
+load_dotenv()
 
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
--- GeminiProcessor.py
+++ GeminiProcessor.py
@@ -5,7 +5,7 @@ import os
 from Recommendation import Recommendation 
 
 def processData(location):
-    #load_dotenv()
+    load_dotenv()
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=GEMINI_API_KEY)
 
